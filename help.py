5ddb1e97fe02f2d8341cdd6f1c5e78a9c6d8d128
Kenheat <kenneth_mo1@hotmail.com>
2025-06-28 00:06:44 +0200

    deleted due to gathering game related actions/functions in one file

--- help.py
+++
@@ -1,6 +0,0 @@
-def help():
-    print("Exit: exit game")
-    print("I: show inventory")
-    print("S: show available backpack space")
-    print("A: add item to backpack")
-    print("R: remove item from backpack")
