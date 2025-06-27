5717848ef2c84c8919476c38437852d6c9cca0cd
Kenheat <kenneth_mo1@hotmail.com>
2025-06-28 00:02:55 +0200

    commented out if statement that adds items if A is pressed; new key input and functionality for looting

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from backpack import *
-from help import *
+from game_actions import *
 
 def main():
     print("Welcome to Loot Game!")
@@ -23,13 +23,24 @@ def main():
         if user_input == "S":
             bp.show_available_backpack_space()
 
-        if user_input == "A":
-            item_to_add = input("Item to add: ")
-            bp.add_item(item_to_add)
+        # Not useful at this stage.
+        #if user_input == "A":
+            #item_to_add = input("Item to add: ")
+            #bp.add_item(item_to_add)
 
         if user_input == "R":
-            item_number = int(input("Item to remove: "))
+            item_number = int(input("Item to remove: \n> "))
             bp.remove_item(item_number)
+        
+        if user_input == "L":
+            looted_item = loot()
+            print(f"Looted item: {looted_item}")
+            keep_or_discard = input("Keep (K) or discard (D) item? \n> ")
+            if keep_or_discard == "K":
+                bp.add_item(looted_item)
+                print("Item stored in backpack.")
+            else:
+                print("Item discarded.")
 
 if __name__ == "__main__":
     main()
