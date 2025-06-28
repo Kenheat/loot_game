0e014452d4be8b47012fa27efd4ca9491f5fa1bc
Kenheat <kenneth_mo1@hotmail.com>
2025-06-28 12:01:47 +0200

    implemented dictionary system for item storage

--- backpack.py
+++ backpack.py
@@ -1,21 +1,20 @@
 class Backpack():
     def __init__(self):
-        self.items = []
-        self.max_backpack_space = 5
-        self.available_backpack_space = self.max_backpack_space - len(self.items)
+        self.empty = "(Empty)"
+        self.items = {1: self.empty,
+                      2: self.empty,
+                      3: self.empty,
+                      4: self.empty,
+                      5: self.empty}
+        self.max_backpack_space = len(self.items)
+        self.available_backpack_space = len(self.items)
     
     def show_items(self):
         print("Items in backpack:\n")
-        item_index = 1
-        for i in range(5):
+        for item in self.items:
             print(" ---")
-            try:
-                print(f"| {item_index} | {self.items[i]}")
-                print(" ---")
-            except:
-                print(f"| {item_index} | (Empty)")
-                print(" ---")
-            item_index += 1
+            print(f"| {item} | {self.items[item]}")
+            print(" ---")
     
     def show_available_backpack_space(self):
         print(f"Available backpack space: {self.available_backpack_space}")
@@ -24,14 +23,17 @@ class Backpack():
         if len(self.items) == 0:
             print("No items in backpack.")
             return
-        print(f"{self.items[item_number - 1]} removed from backpack.")
-        del self.items[item_number - 1]
+        print(f"{self.items[item_number]} removed from backpack.")
+        self.items[item_number] = self.empty
         self.available_backpack_space += 1
     
-    def add_item(self, item):
+    def add_item(self, looted_item):
         if self.available_backpack_space == 0:
             print("No more available space in the backpack.")
             return
-        self.items.append(item)
-        print("Item stored in backpack.")
-        self.available_backpack_space -= 1
+        for item in self.items:
+            if self.items[item] is self.empty:
+                self.items[item] = looted_item
+                print("Item stored in backpack.")
+                self.available_backpack_space -= 1
+                return
