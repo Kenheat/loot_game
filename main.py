647919c1f205a94148b6cadf0d880f587301fd79
Kenheat <kenneth_mo1@hotmail.com>
2025-06-28 11:59:00 +0200

    removed add option 'A' and refined functionality for loot 'L' option

--- main.py
+++ main.py
@@ -23,23 +23,16 @@ def main():
         if user_input == "S":
             bp.show_available_backpack_space()
 
-        # Not useful at this stage.
-        #if user_input == "A":
-            #item_to_add = input("Item to add: ")
-            #bp.add_item(item_to_add)
-
         if user_input == "R":
             item_number = int(input("Item to remove: \n> "))
             bp.remove_item(item_number)
         
         if user_input == "L":
             looted_item = loot()
-            print(f"Looted item: {looted_item}")
             keep_or_discard = input("Keep (K) or discard (D) item? \n> ")
             if keep_or_discard == "K":
                 bp.add_item(looted_item)
-                print("Item stored in backpack.")
-            else:
+            if keep_or_discard == "D":
                 print("Item discarded.")
 
 if __name__ == "__main__":
