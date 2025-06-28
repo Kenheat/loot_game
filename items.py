4ca1c61790880600416c2023efe91d69e2607539
Kenheat <kenneth_mo1@hotmail.com>
2025-06-28 11:54:22 +0200

    tidied code and added Chestpiece class

--- items.py
+++ items.py
@@ -10,8 +10,7 @@ class Sword():
         middle = " of the "
         suffix = ["Boar", "Bear", "Tiger", "Monkey", "Eagle", "Crocodile"]
 
-        name = prefix[random.randrange(0, 6)] + middle + suffix[random.randrange(0, 6)]
-
+        name = prefix[random.randrange(len(prefix))] + middle + suffix[random.randrange(len(suffix))]
         return name
     
     def __str__(self):
@@ -25,8 +24,21 @@ class Potion():
     def generate_name(self):
         potion_type = ["Health", "Mana", "Strength", "Stamina", "Agility", "Intelligence"]
 
-        name = potion_type[random.randrange(0, 6)] + " potion"
+        name = potion_type[random.randrange(len(potion_type))] + " potion"
+        return name
 
+    def __str__(self):
+        return self.name
+
+class Chestpiece():
+    def __init__(self):
+        self.item_type = "Armor"
+        self.name = self.generate_name()
+    
+    def generate_name(self):
+        suffix = ["Boar", "Bear", "Tiger", "Monkey", "Eagle", "Crocodile"]
+
+        name = "Chestplate of the " + suffix[random.randrange(len(suffix))]
         return name
 
     def __str__(self):
