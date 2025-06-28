2940c02735a5b346736031da2abf20f08815317d
Kenheat <kenneth_mo1@hotmail.com>
2025-06-28 11:57:12 +0200

    help function: removed add option, added loot options and rearranged overall order; loot function: randomization of what item gets looted

--- game_actions.py
+++ game_actions.py
@@ -1,12 +1,17 @@
-from items import *
+from items import Sword, Potion, Chestpiece
+import random
 
 def help():
     print("Exit: exit game")
     print("I: show inventory")
     print("S: show available backpack space")
-    print("A: add item to backpack")
+    print("L: loot item")
+    print("   K: keep looted item")
+    print("   D: discard looted item")
     print("R: remove item from backpack")
 
 def loot():
-    item = Potion()
-    return item
+    items = [Sword(), Potion(), Chestpiece()]
+    looted_item = items[random.randrange(len(items))]
+    print(f"Looted item: {looted_item}")
+    return looted_item
