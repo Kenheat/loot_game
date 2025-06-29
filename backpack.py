6f7302a2f642644566820475ec1bda3f17012d97
Kenheat <kenneth_mo1@hotmail.com>
2025-06-29 11:33:06 +0200

    removed_item(): checks for fully empty backpack, empty slots, and invalid command choice; new method loot_to_add: handles looted items returned from loot(). Gives player option to choice which items to add or discard. May make this method more sophisticated, see comments in code.

--- backpack.py
+++ backpack.py
@@ -10,30 +10,80 @@ class Backpack():
         self.available_backpack_space = len(self.items)
     
     def show_items(self):
-        print("Items in backpack:\n")
+        print("\nItems in backpack:\n")
         for item in self.items:
             print(" ---")
             print(f"| {item} | {self.items[item]}")
             print(" ---")
+        print("")
     
     def show_available_backpack_space(self):
         print(f"Available backpack space: {self.available_backpack_space}")
     
-    def remove_item(self, item_number):
-        if len(self.items) == 0:
-            print("No items in backpack.")
+    def remove_item(self):
+        empty_count = 0
+        for item in self.items:
+            if self.items[item] is self.empty:
+                empty_count += 1
+        if empty_count == 5:
+            print("\nNo items in backpack.\n")
             return
-        print(f"{self.items[item_number]} removed from backpack.")
-        self.items[item_number] = self.empty
-        self.available_backpack_space += 1
+        
+        try:
+            item_number = int(input("\nItem to remove: \n> "))
+            if self.items[item_number] is self.empty:
+                print("\nSelected slot is empty.\n")
+                return
+
+            print(f"\n{self.items[item_number]} removed from backpack.\n")
+            self.items[item_number] = self.empty
+            self.available_backpack_space += 1
+        except:
+            print("\nInvalid choice.\n")
     
     def add_item(self, looted_item):
         if self.available_backpack_space == 0:
-            print("No more available space in the backpack.")
+            print("\nNo more available space in the backpack.\n")
             return
         for item in self.items:
             if self.items[item] is self.empty:
                 self.items[item] = looted_item
-                print("Item stored in backpack.")
+                print(f"\n{looted_item} stored in backpack.\n")
                 self.available_backpack_space -= 1
                 return
+
+    def loot_to_add(self, looted_items):
+        # TODO: want the player to have the option of removing items from the backpack while looting
+        while True:
+            # problem(?): if the backpack is full, the option to loot is removed
+            if self.available_backpack_space == 0:
+                print("No more available space in the backpack.")
+                return
+
+            if len(looted_items) == 0:
+                print("No more items in loot.\n")
+                return
+
+            # showing loot content
+            print("Loot:") 
+            for item in looted_items:
+                print(" ---")
+                print(f"| {item} | {looted_items[item]}")
+                print(" ---")
+
+            number_or_discard = input("Choose item to keep (item number) or discard all/rest of items (d).\n> ")
+
+            if number_or_discard == "d":
+                print("Items discarded.")
+                return
+            
+            else:
+                try:
+                    number = int(number_or_discard)
+                    popped_item = looted_items.pop(number)
+                    self.add_item(popped_item) 
+                    # problem if option of removing items from backpack while looting is implemented:
+                    # if player tries to add item to a full bag, the item gets removed from loot (popped) either way
+                    # and may end the loop since there's no more items in loot to deal with
+                except:
+                    print("Invalid choice.\n")
