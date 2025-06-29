9869a4c79143fc44cb03ff3cb6186a274c76e62d
Kenheat <kenneth_mo1@hotmail.com>
2025-06-29 16:24:12 +0200

    new function for start message; general tidying of print messages

--- game_actions.py
+++ game_actions.py
@@ -1,14 +1,24 @@
 from items import Sword, Potion, Chestpiece
 import random
 
+def start_message():
+    print("")
+    print("Welcome to Loot Game!")
+    print("")
+    print('Type "E" to exit game.')
+    print('Type "H" for help menu.')
+    print("")
+
 def help():
+    print("")
     print("E: exit game")
     print("i: show inventory")
     print("s: show available backpack space")
     print("l: loot item")
-    print("   k: keep looted item")
+    print("   <item number>: add chosen item to backpack")
     print("   d: discard looted item")
     print("r: remove item from backpack")
+    print("")
 
 def loot():
     items = [Sword(), Potion(), Chestpiece()]
@@ -20,7 +30,7 @@ def loot():
         looted_items[i + 1] = (items[random.randrange(len(items))])
 
     if len(looted_items) == 0:
-        print("No loot.")
+        print("\nNo loot, try again!\n")
         return None
     
     return looted_items
