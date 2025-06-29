fa78bc1ab216c2b19b86b37bde1c8be5ad50ddf6
Kenheat <kenneth_mo1@hotmail.com>
2025-06-29 11:02:28 +0200

    help(): changed command characters; loot(): now generates random number of random item(s)

--- game_actions.py
+++ game_actions.py
@@ -2,16 +2,25 @@ from items import Sword, Potion, Chestpiece
 import random
 
 def help():
-    print("Exit: exit game")
-    print("I: show inventory")
-    print("S: show available backpack space")
-    print("L: loot item")
-    print("   K: keep looted item")
-    print("   D: discard looted item")
-    print("R: remove item from backpack")
+    print("E: exit game")
+    print("i: show inventory")
+    print("s: show available backpack space")
+    print("l: loot item")
+    print("   k: keep looted item")
+    print("   d: discard looted item")
+    print("r: remove item from backpack")
 
 def loot():
     items = [Sword(), Potion(), Chestpiece()]
-    looted_item = items[random.randrange(len(items))]
-    print(f"Looted item: {looted_item}")
-    return looted_item
+    looted_items = {}
+
+    # randomizing number of items and items in loot
+    random_range_number = random.randrange(4)
+    for i in range(random_range_number):
+        looted_items[i + 1] = (items[random.randrange(len(items))])
+
+    if len(looted_items) == 0:
+        print("No loot.")
+        return None
+    
+    return looted_items
